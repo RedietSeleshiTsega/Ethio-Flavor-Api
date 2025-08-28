f8703d51bf409a18f74f81274fdfb5ee567bf255
Rediet Tsega <redietseleshii@gmail.com>
2025-08-28 16:52:44 +0300

    Added endpoints for categories, ingredients, and cultural tags

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.RecipeListCreateView.as_view(), name='recipe-list-create'),
     path('<int:pk>/', views.RecipeDetailView.as_view(), name='recipe-detail'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('ingredients/', views.IngredientListView.as_view(), name='ingredient-list'),
+    path('cultural-tags/', views.CulturalTagListView.as_view(), name='cultural-tag-list'),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,17 @@
-# recipes/views.py
 from rest_framework import generics, permissions
 from .models import Recipe
-from .serializers import RecipeSerializer
+from .serializers import RecipeSerializer, CategorySerializer, IngredientSerializer, CulturalTagSerializer
+from .models import Category, Ingredient, CulturalTag
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class RecipeListCreateView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all().order_by('-created_at')
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'cultural_tags']
+    search_fields = ['title', 'ingredients__name']
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -23,3 +28,15 @@ class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
                 return Recipe.objects.all()
             return Recipe.objects.filter(user=user)
         return Recipe.objects.all()
+    
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+
+class IngredientListView(generics.ListAPIView):
+    queryset = Ingredient.objects.all().order_by('name')
+    serializer_class = IngredientSerializer
+
+class CulturalTagListView(generics.ListAPIView):
+    queryset = CulturalTag.objects.all().order_by('name')
+    serializer_class = CulturalTagSerializer 
